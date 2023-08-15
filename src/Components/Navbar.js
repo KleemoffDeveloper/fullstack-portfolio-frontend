9346df4772eca395b3cacfd961704ebfa4cbaa18
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-08-15 16:13:11 -0400

    updated readme

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,7 @@ import './Navbar.css'
 export default function Navbar() {
     return (
         <div id="Navbar">
+            <img src='https://github.com/KleemoffDeveloper/fullstack-portfolio-frontend/blob/main/public/Spider-Verse%20logo.png?raw=true'/>
         </div>
     )
 }
