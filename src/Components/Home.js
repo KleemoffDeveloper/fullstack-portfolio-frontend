c6573eea5a41fe8eb8bb206605b2a5bf68830733
KleemoffDeveloper <siliskleemoff@pursuit.org>
2023-08-17 21:22:57 -0400

    updated css

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,29 @@
-import './Home.css';
+import { useEffect, useState } from "react";
+import "./Home.css";
 
-export default function Home(){
-    return (
-        <div id="Home">
-            
-        </div>
-    )
+export default function Home() {
+  const [characterList, setCharacterList] = useState([]);
+  return (
+    <div id="Home">
+      <div id="Welcome-Message">
+        <h1>
+          Welcome to the <span className="Main-Title">Spider-Verse</span>.
+        </h1>
+        <p>View or create new Spider People!</p>
+      </div>
+      <ul id="Home-Display">
+        {characterList.length > 0 ? (
+          characterList.map((character) => <div></div>)
+        ) : (
+          <div id="Home-Loading">
+            <img
+              className="rotate"
+              src={require("./Icons/loading-svgrepo-com.png")}
+            />
+            <p>Loading Spider People...</p>
+          </div>
+        )}
+      </ul>
+    </div>
+  );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,8 +4,13 @@ export default function Navbar() {
   return (
     <div id="Navbar">
       <div className="Logo">
-        <img src="https://github.com/KleemoffDeveloper/fullstack-portfolio-frontend/blob/main/public/Spider-Verse%20logo.png?raw=true"/>
+        <img src="https://github.com/KleemoffDeveloper/fullstack-portfolio-frontend/blob/main/public/Spider-Verse%20logo.png?raw=true" />
         <h2>Spider-Verse</h2>
+      </div>
+      
+      <div className="Buttons">
+        <button>View</button>
+        <button>Create</button>
       </div>
     </div>
   );
